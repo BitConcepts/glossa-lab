df652904e2035a08961dc50b1329a847188ac3a1
Tristen Pierson <tpierson@bitconcepts.tech>
2026-05-24 13:08:40 -0400

    chore: remove stale _fc3.cmd temp file

--- _fc3.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\trist\Development\BitConcepts\glossa-lab
-python -c "import sys,os,asyncio; sys.path.insert(0,'backend'); os.environ['GLOSSA_DATA_DIR']='backend/data'; from glossa_lab.api.foundation_check import run_foundation_check; r=asyncio.run(run_foundation_check()); s=r['summary']; print('PASS=%d FAIL=%d WARN=%d -> %s'%(s['n_pass'],s['n_fail'],s['n_warn'],s['overall_status']))"
